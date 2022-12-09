b418e489aa04804defdbe2fb855a02d0db692cce
kondi7 <wilkkonrad98@gmail.com>
2022-12-10 00:36:01 +0100

    ćwiczenie 2

---
+++ Odd or Even.py
@@ -0,0 +1,8 @@
+# CEL: WYŚWIETL CZY PODANA PRZEZ UŻYTKOWNIKA LICZBA JEST PARZYSTA LUB NIEPARZYSTA
+
+dzielna = int(input("Wpisz dzielna: "))
+
+if dzielna % 2 == 0:
+    print("Liczba parzysta")
+else:
+    print("Liczba nieparzysta")
