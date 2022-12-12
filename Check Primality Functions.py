43bd32b9636c37ffab6d94a99f0552fb6f35c19c
kondi7 <wilkkonrad98@gmail.com>
2022-12-12 22:24:02 +0100

    cwiczenie 11 ulepszonev2

--- Check Primality Functions.py
+++ Check Primality Functions.py
@@ -1,22 +1,18 @@
 # CEL: Zapytaj użytkownika o liczbę i określ, czy jest to liczba pierwsza, czy nie.
 
+from math import sqrt
+
+
 def get_pierwsza(x):
     return int(input(x))
 
 
 while True:
     liczba = get_pierwsza("Podaj liczbę by sprawdzić czy to liczba pierwsza: ")
-    # a = [i for i in range(2, liczba) if liczba > 0 if liczba % i == 0]
-    b = []
-    c = [1, liczba]
-    if liczba > 0:
-        for i in range(1, liczba + 1):
-            if liczba % i == 0:
-                b.append(i)
-                print(b)
-                if len(b) > 2:
-                    print(liczba, "nie jest liczba pierwsza")
-                    break
-                elif b == c:
-                    print(liczba, "jest liczba pierwsza")
-                    break
+
+    if liczba > 1:
+        a = [i for i in range(2, int(sqrt(liczba + 1))) if liczba % i == 0]
+        if len(a) == 0:
+            print(liczba, "jest liczbą pierwszą")
+        else:
+            print(liczba, "jest liczbą złożoną")
