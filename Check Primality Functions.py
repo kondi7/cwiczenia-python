dcf889dc82f2526050d7159966dea59326203fc2
kondi7 <wilkkonrad98@gmail.com>
2022-12-12 20:09:17 +0100

    cwiczenie 11 ulepszone

--- Check Primality Functions.py
+++ Check Primality Functions.py
@@ -6,10 +6,15 @@ def get_pierwsza(x):
 
 while True:
     liczba = get_pierwsza("Podaj liczbę by sprawdzić czy to liczba pierwsza: ")
-    a = [i for i in range(2, liczba) if liczba > 0 if liczba % i == 0]
-
-    if len(a) == 0:
-        print(liczba, "to liczba pierwsza")
-        break
-    else:
-        print(liczba, "nie jest liczbą pierwszą lub jest mniejsza od zera")
+    # a = [i for i in range(2, liczba) if liczba > 0 if liczba % i == 0]
+    b = []
+    if liczba > 0:
+        for i in range(2, liczba):
+            if liczba % i == 0:
+                b.append(i)
+                if len(b) == 1:
+                    print(liczba, "nie jest liczba pierwsza")
+                    break
+                elif len(b) == 0:
+                    print(liczba, "jest liczba pierwsza")
+                    break
